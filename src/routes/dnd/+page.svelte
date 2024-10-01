1372bd92dcb13e1a46ff75b5d1db740fd7ce6ad9
robinafro <oble.ro.2023@skola.ssps.cz>
2024-10-01 02:01:49 +0200

    Frotting

---
+++ src/routes/dnd/+page.svelte
@@ -0,0 +1,86 @@
+<script>
+	import Page from '../+page.svelte';
+</script>
+
+<div class="flex justify-center items-center">
+	<div class="mockup-phone h-[650px]">
+		<div class="camera"></div>
+		<div class="display">
+			<div class="artboard artboard-demo phone-1 bg-base-100">
+				<div class="flex flex-col gap-4 justify-center p-4 w-full">
+					<div class="blur-sm flex justify-between gap-4 p-4 bg-base-200 w-full">
+						<p class="text-sm">Lorem ipsum dolor</p>
+
+						<!-- toggle slide switch -->
+						<label class="inline-flex items-center cursor-pointer">
+							<input type="checkbox" value="" class="sr-only peer" />
+							<div
+								class="relative w-11 h-6 dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after: after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-accent"
+							></div>
+						</label>
+					</div>
+
+					<div class="flex justify-between gap-4 p-4 bg-base-200 w-full">
+						<p class="text-sm">Automaticky zapnout režim Nerušit</p>
+
+						<!-- toggle slide switch -->
+						<label class="inline-flex items-center cursor-pointer">
+							<input type="checkbox" value="" class="sr-only peer" checked />
+							<div
+								class="relative w-11 h-6 dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after: after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-accent"
+							></div>
+						</label>
+					</div>
+
+					<div class="blur-sm flex justify-between gap-4 p-4 bg-base-200 w-full">
+						<p class="text-sm">womp womp waah wah</p>
+
+						<!-- toggle slide switch -->
+						<label class="inline-flex items-center cursor-pointer">
+							<input type="checkbox" value="" class="sr-only peer" />
+							<div
+								class="relative w-11 h-6 dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after: after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-accent"
+							></div>
+						</label>
+					</div>
+
+					<div class="blur-sm flex justify-between gap-4 p-4 bg-base-200 w-full">
+						<p class="text-sm">Lorem ipsum dolor sit amet</p>
+
+						<!-- toggle slide switch -->
+						<label class="inline-flex items-center cursor-pointer">
+							<input type="checkbox" value="" class="sr-only peer" />
+							<div
+								class="relative w-11 h-6 dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after: after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-accent"
+							></div>
+						</label>
+					</div>
+
+					<div class="blur-sm flex justify-between gap-4 p-4 bg-base-200 w-full">
+						<p class="text-sm">Lorem ipsum dolor sit amet</p>
+
+						<!-- toggle slide switch -->
+						<label class="inline-flex items-center cursor-pointer">
+							<input type="checkbox" value="" class="sr-only peer" checked />
+							<div
+								class="relative w-11 h-6 dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after: after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-accent"
+							></div>
+						</label>
+					</div>
+
+					<div class="blur-sm flex justify-between gap-4 p-4 bg-base-200 w-full">
+						<p class="text-sm">Lorem ipsum dolor sit amet</p>
+
+						<!-- toggle slide switch -->
+						<label class="inline-flex items-center cursor-pointer">
+							<input type="checkbox" value="" class="sr-only peer" />
+							<div
+								class="relative w-11 h-6 dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after: after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-accent"
+							></div>
+						</label>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
